45616e24a8228b5ba5bf0fc219855d280d07a324
hadryyassine <hadryyassine@gmail.com>
2023-10-22 10:39:12 +0100

    Create authentification token

---
+++ auth.js
@@ -0,0 +1,33 @@
+const inquirer = require("inquirer");
+const { Octokit } = require("@octokit/rest");
+
+async function authentificate() {
+  const question = [
+    {
+      name: "Github-Token",
+      type: "input",
+      message: "Enter your Github Token",
+      validate: function (value) {
+        if (value.length == 40) {
+          return true;
+        } else {
+          return "Please enter a valid token";
+        }
+      },
+    },
+  ];
+
+  const answer = await inquirer.prompt(question);
+
+  try {
+    const octokit = new Octokit({
+      auth: answer("Github-Token"),
+    });
+    return octokit;
+  } catch (error) {
+    console.log("Error while trying to authentificate");
+    console.log(error);
+  }
+}
+
+module.exports(authentificate);
